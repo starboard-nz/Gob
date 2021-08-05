e67168bcb34073621d0ab6dab84a15989cbed0a4
Laszlo (Laca) Peter <laszlo.peter@xerra.nz>
2021-08-05 22:59:38 +1200

    rename module to gitlab.com/xerra/common/gob

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module encoding/gob
+module gitlab.com/xerra/common/gob
 
 go 1.16
--- type_test.go
+++ type_test.go
@@ -172,7 +172,7 @@ func TestRegistrationNaming(t *testing.T) {
 		name string
 	}{
 		{&N1{}, "*gob.N1"},
-		{N2{}, "encoding/gob.N2"},
+		{N2{}, "gitlab.com/xerra/common/gob.N2"},
 	}
 
 	for _, tc := range testCases {
